94c77b509b34c13ea6c3069efe49461e65e73c61
Abdelouahed-SENANE <abdosenane75@gmail.com>
2024-11-12 11:46:40 +0100

    the first commit to my project

---
+++ wrm/src/test/java/ma/youcode/wrm/WrmApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.youcode.wrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
